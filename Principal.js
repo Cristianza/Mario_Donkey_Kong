18ec10341c77144de170974f03b384273f434f44
Cristianza <lazarod@uninorte.edu.co>
2017-11-19 19:30:09 -0500

    l

--- Principal.js
+++ Principal.js
@@ -105,7 +105,7 @@ var estadoprincipal = {
 		juego.load.audio('salto', 'Sonidos/Salto.mp3');
 		juego.load.audio('moneda', 'Sonidos/Moneda.mp3');
 		juego.load.audio('pierde', 'Sonidos/Pierde.mp3');
-		juego.load.audio('inicial', 'Sonidos/Principial.mp3');
+		juego.load.audio('inicial', 'Sonidos/Principal.mp3');
 	},
 
 	create: function(){
